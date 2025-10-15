a8e1304b45daf31187f6e6de7a93cf5f4455bfc2
anonsilo <67618479+hibbaahmed@users.noreply.github.com>
2025-10-14 22:45:22 -0400

    rm unnecessary code

--- lib/agents/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./seo-research-agent";
-export * from "./content-writer-agent";
-export * from "./competitive-analysis-agent";
-export * from "./image-search-agent";
-export * from "./todo-agent";
-export * from "./router";
-
-
