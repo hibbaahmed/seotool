d869c05d814dad4cad43435a72fcf42bd74a6774
anonsilo <67618479+hibbaahmed@users.noreply.github.com>
2025-10-17 01:44:18 -0400

    fix

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,4 +1,4 @@
-import { Nav } from '@/components/Nav'
+import Nav from '../../components/Nav'
 import { Footer } from '@/components/layout/Footer'
 import { Button } from '@/components/ui/Button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/Card'
